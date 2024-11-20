ac01a866ccaeb0d4f4ff08a0541c20d51d5b4f61
Ekbladh <alwin.ekbladh@gmail.com>
2024-11-20 14:42:56 +0100

    Organized code heavily through making character, trains and win/loss text object into classes. Also stopped train movement. Changed name from scoresAndLives to just hud.

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 let tracksPos = { x: 0, y: 0 };
-let x = -400;
+let x = 0;
 let state = "game";
 
 function setup() {
@@ -21,44 +21,36 @@ function startScreen() {
   push();
   translate(-600, -2060);
   scale(3);
-  entrance();
+  stationEntrance();
   pop();
 }
 
 function gameScreen() {
+  hud();
+
   trainStation();
-  scoresAndLives();
+
+  trainTracks((tracksPos.x = 0), (tracksPos.y = 160));
+  trainTracks((tracksPos.x = 0), (tracksPos.y = 290));
+  trainTracks((tracksPos.x = 0), (tracksPos.y = 460));
+  trainTracks((tracksPos.x = 0), (tracksPos.y = 620));
+  trainTracks((tracksPos.x = 0), (tracksPos.y = 700));
+  trainTracks((tracksPos.x = 0), (tracksPos.y = 860));
+
+  player.character();
+  hobo.character();
+
+  trainOne.train();
+  trainTwo.train();
+  trainThree.train();
+  trainFour.train();
+  trainFive.train();
+  trainSix.train();
+
+  stationEntrance();
 }
 
-function winScreen() {
-  trainTracksCall();
-  gameScreen();
-  entrance();
-
-  textSize(70);
-  textAlign(CENTER);
-  strokeWeight(0);
-  fill(255);
-  text("You won!", 300, 530);
-  textSize(22);
-  text("Press space to try again", 300, 590);
-}
-
-function lossScreen() {
-  trainTracksCall();
-  gameScreen();
-  entrance();
-
-  textSize(70);
-  textAlign(CENTER);
-  strokeWeight(0);
-  fill(255);
-  text("You lost!", 300, 530);
-  textSize(22);
-  text("Press space to try again", 300, 590);
-}
-
-function scoresAndLives() {
+function hud() {
   strokeWeight(0);
   textAlign(LEFT);
   fill(0);
@@ -73,7 +65,127 @@ function scoresAndLives() {
   text("SCORE:", 470, 25);
 }
 
-function entrance() {
+function callingStatesWithSpaceBar() {
+  //Using the spacebar lets you navigate game menus
+  if (keyIsDown(32) && state === "start") {
+    state = "game";
+  } else if (keyIsDown(32) && state === "win") {
+    state = "game";
+  } else if (keyIsDown(32) && state === "loss") {
+    state = "game";
+  }
+}
+
+class ScreenText {
+  constructor(topText, bottomText) {
+    this.topText = topText;
+    this.bottomText = bottomText;
+  }
+
+  screenText() {
+    textSize(70);
+    textAlign(CENTER);
+    strokeWeight(0);
+    fill(255);
+    text(this.topText, 300, 530);
+    textSize(22);
+    text(this.bottomText, 300, 590);
+  }
+}
+
+class Train {
+  constructor(x, y, trainColor, carAmount) {
+    this.x = x;
+    this.y = y;
+    this.trainColor = trainColor;
+    this.carAmount = carAmount;
+  }
+
+  train() {
+    let x = this.x;
+    let y = this.y;
+    strokeWeight(0);
+
+    fill(228, 204, 91);
+    ellipse(x + 250, y + 12, 10, 10);
+    ellipse(x + 250, y - 12, 10, 10);
+    fill(228, 204, 91, 127);
+    quad(x + 250, y - 15, x + 370, y - 20, x + 370, y + 20, x + 250, y + 15);
+    fill(this.trainColor);
+    ellipse(x + 210, y + 0, 95, 60); // Train front body
+
+    if (this.carAmount >= 1) {
+      fill(50);
+      rect(x - 0, y - 4, 200, 8); //Thing connecting the train
+      fill(this.trainColor);
+      rect(x + 0, y - 30, 125, 60, 5); // middle train car
+      fill(20);
+      rect(x + 12, y - 25, 100, 50, 5); // coal middle train
+    }
+    if (this.carAmount >= 2) {
+      fill(50);
+      rect(x - 300, y - 4, 300, 8); //Thing connecting the train
+      fill(this.trainColor);
+      rect(x - 140, y - 30, 125, 60, 5); // Back train car
+      fill(20);
+      rect(x - 127, y - 25, 100, 50, 5); // coal back train
+    }
+
+    fill(20);
+    ellipse(x + 210, y + 0, 65, 40); // Glass
+
+    fill(this.trainColor);
+    rect(x + 140, y - 30, 70, 60, 5); // Train back body
+    rect(x + 185, y - 22, 40, 44); // Glass cover
+  }
+}
+
+class Character {
+  constructor(x, y, shirtColor, skinColor, pantsColor, beerCan) {
+    this.x = x;
+    this.y = y;
+    this.shirtColor = shirtColor;
+    this.skinColor = skinColor;
+    this.pantsColor = pantsColor;
+    this.beerCan = beerCan;
+  }
+
+  character() {
+    let x = this.x;
+    let y = this.y;
+    strokeWeight(0);
+    fill(52, 56, 48);
+    ellipse(x - 15, y - 20, 10, 10); // shoe front
+    rect(x + 10, y + 20, 10, 5); // shoe back
+    fill(this.pantsColor);
+    quad(x - 20, y - 20, x - 10, y - 20, x + 0, y + 0, x - 20, y + 0); // leg front
+    quad(x + 20, y + 20, x + 10, y + 20, x + 0, y + 0, x + 20, y + 0); // leg back
+    fill(this.shirtColor);
+    ellipse(x + 0, y + 0, 60, 20); // body
+    ellipse(x + 27, y - 7, 10, 20); // front arm
+    ellipse(x - 27, y + 7, 10, 20); // back arm
+    fill(this.skinColor);
+    ellipse(x - 27, y + 15, 8, 5); // back hand
+    ellipse(x + 27, y - 15, 8, 5); // front hand
+    ellipse(x - 12, y + 0, 5, 5); // left ear
+    ellipse(x + 12, y + 0, 5, 5); // right ear
+    fill(48, 44, 52);
+    ellipse(x + 0, y + 0, 25, 25); // head
+
+    if (this.beerCan) {
+      fill(64, 156, 52);
+      rect(x + 20, y - 20, 10, 6);
+      rect(x + 26, y - 19, 10, 3);
+      fill(9, 88, 36);
+      rect(x + 22, y - 19, 6, 4);
+      rect(x + 35, y - 20, 2, 5);
+      fill(212, 18, 23);
+      ellipse(x + 26, y - 17, 2, 2);
+    }
+  }
+}
+
+function stationEntrance() {
   push();
   translate(0, 30);
   strokeWeight(0);
@@ -165,158 +277,47 @@ function trainTracks() {
   }
 }
 
-function trainTracksCall() {
-  trainTracks((tracksPos.x = 0), (tracksPos.y = 160));
-  trainTracks((tracksPos.x = 0), (tracksPos.y = 290));
-  trainTracks((tracksPos.x = 0), (tracksPos.y = 460));
-  trainTracks((tracksPos.x = 0), (tracksPos.y = 620));
-  trainTracks((tracksPos.x = 0), (tracksPos.y = 700));
-  trainTracks((tracksPos.x = 0), (tracksPos.y = 860));
-}
+let win = new ScreenText("You won!", "Press space to try again");
+let loss = new ScreenText("You lost!", "Press space to try again");
 
-function hoboCharacter(x, y) {
-  push();
-  rotate(HALF_PI);
-  fill(52, 56, 48);
-  ellipse(x - 15, y - 20, 10, 10); // shoe front
-  rect(x + 10, y + 20, 10, 5); // shoe back
-  fill(66, 40, 27);
-  quad(x - 20, y - 20, x - 10, y - 20, x + 0, y + 0, x - 20, y + 0); // leg front
-  quad(x + 20, y + 20, x + 10, y + 20, x + 0, y + 0, x + 20, y + 0); // leg back
-  fill(120, 36, 36);
-  ellipse(x + 0, y + 0, 60, 20); // body
-  ellipse(x + 27, y - 7, 10, 20); // front arm
-  ellipse(x - 27, y + 7, 10, 20); // back arm
-  fill(140, 112, 98);
-  ellipse(x - 27, y + 15, 8, 5); // back hand
-  ellipse(x + 27, y - 15, 8, 5); // front hand
-  ellipse(x - 12, y + 0, 5, 5); // left ear
-  ellipse(x + 12, y + 0, 5, 5); // right ear
-  fill(48, 44, 52);
-  ellipse(x + 0, y + 0, 25, 25); // head
-  fill(64, 156, 52);
-  rect(x + 20, y - 20, 10, 6);
-  rect(x + 26, y - 19, 10, 3);
-  fill(9, 88, 36);
-  rect(x + 22, y - 19, 6, 4);
-  rect(x + 35, y - 20, 2, 5);
-  fill(212, 18, 23);
-  ellipse(x + 26, y - 17, 2, 2);
+let trainOne = new Train(100, 210, "rgb(40, 188, 132)", 0);
+let trainTwo = new Train(100, 340, "rgb(120, 36, 36)", 1);
+let trainThree = new Train(100, 510, "rgb(120, 136, 0)", 2);
+let trainFour = new Train(100, 670, "rgb(40, 188, 132)", 0);
+let trainFive = new Train(100, 750, "rgb(120, 36, 36)", 1);
+let trainSix = new Train(100, 910, "rgb(120, 136, 0)", 2);
 
-  pop();
-}
-
-function trainCharacterLong(x, y) {
-  strokeWeight(0);
-  fill(50);
-  rect(x - 120, y - 4, 300, 8); //Thing connecting the train
-  fill(228, 204, 91);
-  ellipse(x + 250, y + 12, 10, 10);
-  ellipse(x + 250, y - 12, 10, 10);
-  fill(228, 204, 91, 127);
-  quad(x + 250, y - 15, x + 370, y - 20, x + 370, y + 20, x + 250, y + 15);
-
-  fill(64, 108, 52);
-  rect(x + 0, y - 30, 125, 60, 5); // middle train car
-  rect(x - 140, y - 30, 125, 60, 5); // Back train car
-  ellipse(x + 210, y + 0, 95, 60); // Train front body
-  fill(20);
-
-  ellipse(x + 210, y + 0, 65, 40); // Glass
-  rect(x + 12, y - 25, 100, 50, 5); // electric connection middle car back
-  rect(x - 127, y - 25, 100, 50, 5); // electric connection back car front
-
-  fill(64, 108, 52);
-  rect(x + 140, y - 30, 70, 60, 5); // Train back body
-  rect(x + 185, y - 22, 40, 44); // Glass cover
-}
-
-function trainCharacterShort(x, y) {
-  fill(50);
-  rect(x + 120, y - 4, 30, 8); //Thing connecting the train
-  fill(228, 204, 91);
-  ellipse(x + 250, y + 12, 10, 10);
-  ellipse(x + 250, y - 12, 10, 10);
-  fill(228, 204, 91, 127);
-  quad(x + 250, y - 15, x + 370, y - 20, x + 370, y + 20, x + 250, y + 15);
-
-  fill(64, 108, 52);
-  rect(x + 0, y - 30, 125, 60, 5); // middle train car
-  //rect(x - 140, y - 30, 125, 60, 5); // Back train car
-  ellipse(x + 210, y + 0, 95, 60); // Train front body
-
-  fill(20);
-  ellipse(x + 210, y + 0, 65, 40); // Glass
-  rect(x + 12, y - 25, 100, 50, 5); // inside of car
-  //rect(x - 127, y - 25, 100, 50, 5); // inside of car
-
-  fill(64, 108, 52);
-  rect(x + 140, y - 30, 70, 60, 5); // Train back body
-  rect(x + 185, y - 22, 40, 44); // Glass cover
-}
-
-function trainCall() {
-  trainCharacterLong(x, 210);
-  trainCharacterLong(x, 510);
-  trainCharacterLong(x, 670);
-  trainCharacterLong(x, 910);
-  trainCharacterShort(x, 340);
-  trainCharacterShort(x, 750);
-}
-
-function mainCharacter(x, y) {
-  fill(52, 56, 48);
-  ellipse(x - 15, y - 20, 10, 10); // shoe front
-  rect(x + 10, y + 20, 10, 5); // shoe back
-  fill(4, 148, 172);
-  quad(x - 20, y - 20, x - 10, y - 20, x + 0, y + 0, x - 20, y + 0); // leg front
-  quad(x + 20, y + 20, x + 10, y + 20, x + 0, y + 0, x + 20, y + 0); // leg back
-  fill(40, 188, 132);
-  ellipse(x + 0, y + 0, 60, 20); // body
-  ellipse(x + 27, y - 7, 10, 20); // front arm
-  ellipse(x - 27, y + 7, 10, 20); // back arm
-  fill(244, 196, 172);
-  ellipse(x - 27, y + 15, 8, 5); // back hand
-  ellipse(x + 27, y - 15, 8, 5); // front hand
-  ellipse(x - 12, y + 0, 5, 5); // left ear
-  ellipse(x + 12, y + 0, 5, 5); // right ear
-  fill(48, 44, 52);
-  ellipse(x + 0, y + 0, 25, 25); // head
-}
-
-function callingStatesWithSpace() {
-  //Using the spacebar lets you navigate game menus
-  if (keyIsDown(32) && state === "start") {
-    state = "game";
-  } else if (keyIsDown(32) && state === "win") {
-    state = "game";
-  } else if (keyIsDown(32) && state === "loss") {
-    state = "game";
-  }
-}
+let player = new Character(
+  50,
+  830,
+  "rgb(40, 188, 132)",
+  "rgb(244, 196, 172)",
+  "rgb(4, 148, 172)",
+  false
+);
+let hobo = new Character(
+  100,
+  300,
+  "rgb(120, 36, 36)",
+  "rgb(140, 112, 98)",
+  "rgb(66, 40, 27)",
+  true
+);
 
 function draw() {
   background(30);
-  callingStatesWithSpace();
+  callingStatesWithSpaceBar();
 
   //What state = to what screens
   if (state === "start") {
     startScreen();
   } else if (state === "game") {
-    trainTracksCall();
-    trainCall();
     gameScreen();
-    mainCharacter(300, 820);
-    hoboCharacter(430, -200);
-    entrance();
-
-    x = x + 8;
-    if (x >= 800) {
-      x = -400;
-    }
   } else if (state === "win") {
-    winScreen();
+    gameScreen();
+    win.screenText();
   } else if (state === "loss") {
-    lossScreen();
+    gameScreen();
+    loss.screenText();
   }
 }
